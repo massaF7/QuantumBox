a077cf5dfbe912a1195d79e9e4dd52a8183b018f
massaF7 <kitaoka.gr@yahoo.co.jp>
2015-10-26 12:19:47 +0900

    python IDEL の仕様の確認と作成のテストです

---
+++ firsttest.py
@@ -0,0 +1,16 @@
+import this
+import this
+print "Hello world!"
+import this
+import this
+import this
+import this
+import this
+print "This is a Quantum study for student in Tamagawa."
+print "So we have to study about Quontum mecanisum for fundamental knowlede and basic mathmatic"
+print "gu html so habu date"
+print "hucking that"
+print "We have to use the language of Python"
+print "to use Deep Learnig"
+print "and adapt the theory of Quontum"
+import this
